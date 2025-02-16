85987c741ab22210955d9cf9992f985e280b025d
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-02-16 19:44:12 -0300

    projeto criado

---
+++ src/test/java/com/projetohexagonal/operadora_cartao_credito/OperadoraCartaoCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetohexagonal.operadora_cartao_credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperadoraCartaoCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
